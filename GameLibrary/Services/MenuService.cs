bd1f794f00d3c3526ef60433fd821957e94fcf24
itishadi <113441103+itishadi@users.noreply.github.com>
2023-06-08 14:43:20 +0200

    FuelService completed and completed the start menu

--- GameLibrary/Services/MenuService.cs
+++ GameLibrary/Services/MenuService.cs
@@ -28,41 +28,51 @@ namespace GameLibrary.Services
             this.fuelService = fuelService;
         }
 
-
         public async Task Start()
         {
             Console.Clear();
             Console.WriteLine("Welcome to Car Simulator!");
-            await apiService.GetAsync();
-
-            while (isRunning)
+            Console.WriteLine("1: Start");
+            Console.WriteLine("2: End");
+            var answer = Console.ReadLine();
+            if (answer == "1")
             {
-                Console.WriteLine("\n*--------------------------------------------------------------*");
-                Console.WriteLine("\n1: Turn left");
-                Console.WriteLine("2: Turn right");
-                Console.WriteLine("3: Drive forward");
-                Console.WriteLine("4: Reverse");
-                Console.WriteLine("5: Take a break");
-                Console.WriteLine("6: Refuel");
-                Console.WriteLine("7: End!");
-                Console.WriteLine("\n*--------------------------------------------------------------*");
-                Console.Write("Enter a command: ");
-
-                string input = Console.ReadLine();
-                string output = commandService.ExecuteCommand(input);
-
                 Console.Clear();
-                Console.WriteLine(output);
-                fatigueService.Fatigue(input);
-                fuelService.Fuel(input);
-                if(input == "7")
+                await apiService.GetAsync();
+
+                while (isRunning)
                 {
-                    isRunning = false;
+                    Console.WriteLine("\n*--------------------------------------------------------------*");
+                    Console.WriteLine("\n1: Turn left");
+                    Console.WriteLine("2: Turn right");
+                    Console.WriteLine("3: Drive forward");
+                    Console.WriteLine("4: Reverse");
+                    Console.WriteLine("5: Take a break");
+                    Console.WriteLine("6: Refuel");
+                    Console.WriteLine("7: Menu!");
+                    Console.WriteLine("\n*--------------------------------------------------------------*");
+                    Console.Write("Enter a command: ");
+
+                    string input = Console.ReadLine();
+                    string output = commandService.ExecuteCommand(input);
+
+                    Console.Clear();
+                    Console.WriteLine(output);
+                    fatigueService.Fatigue(input);
+                    fuelService.Fuel(input);
+                    if (input == "7")
+                    {
+                        await Start();
+                    }
+                    else if (input != "1" && input != "2" && input != "3" && input != "4" && input != "5" && input != "6")
+                    {
+                        Console.WriteLine("Try again!");
+                    }
                 }
-                else if(input != "1" && input != "2" && input != "3" && input != "4" && input != "5" && input != "6") 
-                {
-                    Console.WriteLine("Try again!");
-                }
+            }
+            else
+            {
+                isRunning = false;
             }
         }
     }
