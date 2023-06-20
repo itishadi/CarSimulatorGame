23272aab9c44d4309528c3d7a5dc5ee849ff8ba1
itishadi <113441103+itishadi@users.noreply.github.com>
2023-06-20 15:26:29 +0200

    added two NUnitTests on commandService

---
+++ CarSimulatorGameTest/APIServices/FakeApiServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using GameLibrary.APIModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameLibrary.Tests.APIModels
+{
+    [TestClass]
+    public class ApiResponseTests
+    {
+        [TestMethod]
+        public void ApiResponse_ReturnsNonNullNameAndAge_WhenSet()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse();
+            var result = new Result();
+            result.name = new Name { first = "John", last = "Doe" };
+            result.dob = new Dob { age = 30 };
+            apiResponse.results = new[] { result };
+
+            // Act
+            var name = apiResponse.results[0].name;
+            var age = apiResponse.results[0].dob.age;
+
+            // Assert
+            Assert.IsNotNull(name);
+            Assert.IsNotNull(name.first);
+            Assert.IsNotNull(name.last);
+            Assert.IsFalse(string.IsNullOrEmpty(name.first));
+            Assert.IsFalse(string.IsNullOrEmpty(name.last));
+            Assert.IsTrue(age > 0);
+        }
+    }
+}
