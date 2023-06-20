fd06f49d8f54fbe3fe1cb68b9ffa0a84424ee221
itishadi <113441103+itishadi@users.noreply.github.com>
2023-06-20 10:48:35 +0200

    New project NUnitTests

---
+++ NUnitCarSimulatorGame/Services/NUnitCommandServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using GameLibrary.Models;
+using GameLibrary.Services;
+using NUnit.Framework;
+
+namespace NUnitCarSimulatorGame.Services
+{
+    [TestFixture]
+    public class NUnitCommandServiceTest
+    {
+        private CommandService _commandService;
+        private Car _car;
+
+        [SetUp]
+        public void Setup()
+        {
+            _car = new Car();
+            _commandService = new CommandService(new Driver(), _car);
+        }
+
+        [Test]
+        public void ExecuteCommand_ShouldChangeDirectionToWest_WhenInputIs1AndCurrentDirectionIsNorthward()
+        {
+            // Arrange
+            _car.Direction = "Northward";
+
+            // Act
+            _commandService.ExecuteCommand("1");
+
+            // Assert
+            Assert.AreEqual("West", _car.Direction);
+        }
+
+        [Test]
+        public void ExecuteCommand_ShouldChangeDirectionToEast_WhenInputIs1AndCurrentDirectionIsSouthward()
+        {
+            // Arrange
+            _car.Direction = "Southward";
+
+            // Act
+            _commandService.ExecuteCommand("1");
+
+            // Assert
+            Assert.AreEqual("East", _car.Direction);
+        }
+    }
+}
---
+++ NUnitCarSimulatorGame/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NUnitCarSimulatorGame
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
